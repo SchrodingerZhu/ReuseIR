532b748a045a5918c810c1f1c7c860a4a6317491
anqurvanillapy <anqurvanillapy@gmail.com>
2024-08-20 00:19:58 +0800

    [build] force to use Ninja as CMake generator
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- reuse-mlir/build.rs
+++ reuse-mlir/build.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let dst = cmake::Config::new(".").build().join("build").join("lib");
+    let dst = cmake::Config::new(".")
+        .generator("Ninja")
+        .build()
+        .join("build")
+        .join("lib");
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=MLIRReuseIR");
 }
