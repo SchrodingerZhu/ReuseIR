d486ae249d202803413a79638356071e53469a26
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-08-23 23:34:36 -0700

    [ReuseIR] adjust memory api (#38)

---
+++ reuse-runtime/src/allocator.cpp
@@ -0,0 +1,45 @@
+#include "snmalloc/snmalloc.h"
+
+extern "C" [[gnu::visibility("default")]] void *
+__reuse_ir_alloc_impl(size_t size, size_t alignment) {
+  using namespace snmalloc;
+  return ThreadAlloc::get().alloc(aligned_size(alignment, size));
+}
+
+extern "C" [[gnu::visibility("default")]] void
+__reuse_ir_dealloc_impl(void *ptr, size_t size, size_t alignment) {
+  using namespace snmalloc;
+  ThreadAlloc::get().dealloc(ptr, aligned_size(alignment, size));
+}
+
+extern "C" [[gnu::visibility("default")]] void *
+__reuse_ir_realloc_impl(void *ptr, size_t old_size, size_t alignment,
+                        size_t new_size) {
+  using namespace snmalloc;
+  size_t aligned_old_size = aligned_size(alignment, old_size),
+         aligned_new_size = aligned_size(alignment, new_size);
+  if (size_to_sizeclass_full(aligned_old_size).raw() ==
+      size_to_sizeclass_full(aligned_new_size).raw())
+    return ptr;
+  void *p = ThreadAlloc::get().alloc(aligned_new_size);
+  if (p) {
+    std::memcpy(p, ptr, old_size < new_size ? old_size : new_size);
+    ThreadAlloc::get().dealloc(ptr, aligned_old_size);
+  }
+  return p;
+}
+
+extern "C" [[gnu::visibility("default")]] void *
+__reuse_ir_realloc_nocopy_impl(void *ptr, size_t old_size, size_t alignment,
+                               size_t new_size) {
+  using namespace snmalloc;
+  size_t aligned_old_size = aligned_size(alignment, old_size),
+         aligned_new_size = aligned_size(alignment, new_size);
+  if (size_to_sizeclass_full(aligned_old_size).raw() ==
+      size_to_sizeclass_full(aligned_new_size).raw())
+    return ptr;
+  void *p = ThreadAlloc::get().alloc(aligned_new_size);
+  if (p)
+    ThreadAlloc::get().dealloc(ptr, aligned_old_size);
+  return p;
+}
--- reuse-runtime/src/allocator.rs
+++ reuse-runtime/src/allocator.rs
@@ -1,19 +1,47 @@
 #![allow(clippy::missing_safety_doc)]
-use std::ffi::c_void;
+use std::{
+    alloc::{GlobalAlloc, Layout},
+    ffi::c_void,
+};
+
+#[cfg(feature = "snmalloc")]
+extern "C" {
+    pub fn __reuse_ir_alloc_impl(size: usize, alignment: usize) -> *mut c_void;
+    pub fn __reuse_ir_dealloc_impl(ptr: *mut c_void, size: usize, alignment: usize);
+    pub fn __reuse_ir_realloc_impl(
+        ptr: *mut c_void,
+        old_size: usize,
+        alignment: usize,
+        size: usize,
+    ) -> *mut c_void;
+    pub fn __reuse_ir_realloc_nocopy_impl(
+        ptr: *mut c_void,
+        old_size: usize,
+        alignment: usize,
+        size: usize,
+    ) -> *mut c_void;
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn __reuse_ir_alloc(size: usize, alignment: usize) -> *mut c_void {
-    std::alloc::alloc(std::alloc::Layout::from_size_align_unchecked(
-        size, alignment,
-    )) as _
+    let res = if cfg!(feature = "snmalloc") {
+        __reuse_ir_alloc_impl(size, alignment)
+    } else {
+        std::alloc::alloc(Layout::from_size_align_unchecked(size, alignment)) as _
+    };
+    if res.is_null() {
+        std::alloc::handle_alloc_error(Layout::from_size_align_unchecked(size, alignment));
+    }
+    res
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn __reuse_ir_dealloc(ptr: *mut c_void, size: usize, alignment: usize) {
-    std::alloc::dealloc(
-        ptr as _,
-        std::alloc::Layout::from_size_align_unchecked(size, alignment),
-    )
+    if cfg!(feature = "snmalloc") {
+        __reuse_ir_dealloc_impl(ptr, size, alignment);
+    } else {
+        std::alloc::dealloc(ptr as _, Layout::from_size_align_unchecked(size, alignment));
+    }
 }
 
 #[no_mangle]
@@ -23,9 +51,62 @@ pub unsafe extern "C" fn __reuse_ir_realloc(
     alignment: usize,
     size: usize,
 ) -> *mut c_void {
-    std::alloc::realloc(
-        ptr as _,
-        std::alloc::Layout::from_size_align_unchecked(old_size, alignment),
-        size,
-    ) as _
+    let res = if cfg!(feature = "snmalloc") {
+        __reuse_ir_realloc_impl(ptr, old_size, alignment, size)
+    } else {
+        std::alloc::realloc(
+            ptr as _,
+            Layout::from_size_align_unchecked(old_size, alignment),
+            size,
+        ) as _
+    };
+    if res.is_null() {
+        std::alloc::handle_alloc_error(Layout::from_size_align_unchecked(size, alignment));
+    }
+    res
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn __reuse_ir_realloc_nocopy(
+    ptr: *mut c_void,
+    old_size: usize,
+    alignment: usize,
+    size: usize,
+) -> *mut c_void {
+    let res = if cfg!(feature = "snmalloc") {
+        __reuse_ir_realloc_nocopy_impl(ptr, old_size, alignment, size)
+    } else {
+        std::alloc::realloc(
+            ptr as _,
+            Layout::from_size_align_unchecked(old_size, alignment),
+            size,
+        ) as _
+    };
+    if res.is_null() {
+        std::alloc::handle_alloc_error(Layout::from_size_align_unchecked(size, alignment));
+    }
+    res as _
+}
+
+#[cfg(feature = "snmalloc")]
+mod global {
+    use super::*;
+    struct SnMalloc;
+
+    unsafe impl GlobalAlloc for SnMalloc {
+        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            __reuse_ir_alloc_impl(layout.size(), layout.align()) as _
+        }
+
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+            __reuse_ir_dealloc_impl(ptr as _, layout.size(), layout.align());
+        }
+
+        unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+            __reuse_ir_realloc_impl(ptr as _, layout.size(), layout.align(), new_size) as _
+        }
+    }
+
+    #[global_allocator]
+    static A: SnMalloc = SnMalloc;
 }
